5670e33d6d9212ab0e5795d6af997c40dcfb1178
João Victor de Andrade <joao.victor@primesquad.com.br>
2021-06-12 11:21:50 -0300

    first commit

---
+++ src/main/java/com/unit/course/model/Course.java
@@ -0,0 +1,126 @@
+package com.unit.course.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    private int id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String ementa;
+
+    private Date dataCadastro;
+
+    private Date dataInicio;
+
+    private float valor;
+
+    /**
+     * @return int return the Iid
+     */
+    public int getIid() {
+        return id;
+    }
+
+    /**
+     * @param Iid the Iid to set
+     */
+    public void setIid(int Iid) {
+        this.id = Iid;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return String return the ementa
+     */
+    public String getEmenta() {
+        return ementa;
+    }
+
+    /**
+     * @param ementa the ementa to set
+     */
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    /**
+     * @return Date return the dataCadastro
+     */
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    /**
+     * @param dataCadastro the dataCadastro to set
+     */
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    /**
+     * @return Date return the dataInicio
+     */
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    /**
+     * @param dataInicio the dataInicio to set
+     */
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    /**
+     * @return float return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+}
